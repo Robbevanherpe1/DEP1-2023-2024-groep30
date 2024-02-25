575cf4b97a0d34f91d3ec33d21f50c7ea77ee336
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-02-26 00:28:51 +0100

    Update: fetch_goals beter maken

--- transfermarkt/scripts/fetch_goals.py
+++ transfermarkt/scripts/fetch_goals.py
@@ -27,13 +27,13 @@ def get_match_data(box, seizoen, speeldag):
     for row in rows:
         data = {}
         
-        data['Seizoen'] = str(seizoen) + '/' + str(seizoen + 1)
+        data['Seizoen'] = str(seizoen) + '-' + str(seizoen + 1)
         data['Speeldag'] = speeldag
         
         team_names = row.find_all('td', class_='spieltagsansicht-vereinsname')
         if team_names:
-            data['Home_Team'] = re.sub(r"\(.*?\)", "", team_names[0].get_text(strip=True))
-            data['Away_Team'] = re.sub(r"\(.*?\)", "", team_names[-1].get_text(strip=True))
+            data['Thuisploeg'] = re.sub(r"\(.*?\)", "", team_names[0].get_text(strip=True))
+            data['Uitploeg'] = re.sub(r"\(.*?\)", "", team_names[-1].get_text(strip=True))
 
         result_link = row.find('td', class_='spieltagsansicht-ergebnis').find('a', href=True)
         if result_link:
@@ -43,8 +43,8 @@ def get_match_data(box, seizoen, speeldag):
         if date_time_row:
             date_time = date_time_row.get_text(strip=True).split('-')
             if len(date_time) >= 2:
-                data['Date'] = date_time[0].strip()
-                data['Time'] = date_time[1].strip()
+                data['Datum'] = date_time[0].strip()
+                data['Tijdstip'] = date_time[1].strip()
 
         goal_rows = box.find_all('tr', class_='spieltagsansicht-aktionen')
         for goal_row in goal_rows:
@@ -52,13 +52,13 @@ def get_match_data(box, seizoen, speeldag):
             goal_details = goal_row.find_all('td')
             if goal_details and len(goal_details) >= 5:
                 
-                goal_data['Current_Score'] = goal_details[2].get_text(strip=True)
+                goal_data['ResultaatMomenteel'] = goal_details[2].get_text(strip=True)
                
                 goal_time_values = [goal_details[i].get_text(strip=True).rstrip("'") for i in [1, 3]]
-                goal_data['Goal_Time'] = next((time for time in goal_time_values if time), None)
+                goal_data['GoalTijdstip'] = next((time for time in goal_time_values if time), None)
 
                 goal_scorers = [goal_details[i].find('a', title=True) for i in [0, 4]]
-                goal_data['Scorer'] = next((scorer['title'] for scorer in goal_scorers if scorer is not None), None)
+                goal_data['GoalScorer'] = next((scorer['title'] for scorer in goal_scorers if scorer is not None), None)
                 
                 
                 
@@ -83,22 +83,22 @@ def main():
     
     all_matches = []
 
-    for year in range(STARTJAAR, EINDJAAR):
-        print(f"Processing data for the year: {year}")
+    for jaar in range(STARTJAAR, EINDJAAR):
+        print(f"Processing data for the season: {jaar}-{jaar+1}")
         speeldag = 1
         while True:
-            url = f'{URL}{year}&spieltag={speeldag}'
+            url = f'{URL}{jaar}&spieltag={speeldag}'
             soup = fetch_url(url)
             if not soup.find('option', selected=True, value=str(speeldag)):
-                break  # No more speeldagen for this year
-            matches = process_all_boxes(soup, year, speeldag)
+                break  # geen speeldag meer voor dit seizoen
+            matches = process_all_boxes(soup, jaar, speeldag)
             all_matches.extend(matches)
             speeldag += 1
 
     # Write data to CSV
     with open('goals.csv', 'w', newline='', encoding='utf-8') as file:
-        fieldnames = ['Match_ID', 'Seizoen', 'Speeldag', 'Datum', 'Tijdstip', 'Thuisploeg', 'Uitploeg', 
-                      'ResultaatMomenteel', 'GoalTijdstip', 'GoalMaker']
+        fieldnames = ['Match_ID', 'Seizoen', 'Speeldag', 'Datum', 'Tijdstip', 'Thuisploeg', 'Uitploeg',
+                       'GoalTijdstip', 'GoalScorer', 'ResultaatMomenteel']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         for match in all_matches:
