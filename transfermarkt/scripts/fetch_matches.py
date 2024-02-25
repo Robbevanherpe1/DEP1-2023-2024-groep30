581a60257cccc4e24f0986d9ce755ca59631b062
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-02-25 18:07:54 +0100

    Update: fetch_matches verbeteren

--- transfermarkt/scripts/fetch_matches.py
+++ transfermarkt/scripts/fetch_matches.py
@@ -7,28 +7,36 @@ from datetime import datetime
 import re
 
 
-base_url = "https://www.transfermarkt.be/jupiler-pro-league/spieltag/wettbewerb/BE1/plus/"
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
+URL = "https://www.transfermarkt.be/jupiler-pro-league/spieltag/wettbewerb/BE1/plus/"
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
 
-startjaar = 2023  # Beginjaar
-eindjaar = datetime.now().year - 1  # Huidig jaar
+STARTJAAR = 2023  # Beginjaar
+EINDJAAR = datetime.now().year - 1  # Huidig jaar
 
 
-startspeeldag = 1
-eindspeeldag = 50
+STARTSPEELDAG = 1
+EINDSPEELDAG = 50
 
 with open('matches.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=['Seizoen', 'Speeldag', 'Datum', 'Tijdstip', 'Naam thuisploeg', 'Resultaat thuisploeg', 'Resultaat uitploeg', 'Naam uitploeg','Match-ID'])
+    writer = csv.DictWriter(file, fieldnames=['Match_ID', 'Seizoen', 'Speeldag', 'Datum', 'Tijdstip', 'Thuisploeg', 
+                                              'ResultaatThuisploeg', 'ResultaatUitploeg', 'Uitploeg'])
     writer.writeheader()
 
-    for year in range(startjaar, eindjaar + 1):
-        for speeldag in range(startspeeldag, eindspeeldag - 1):  # Maximaal aantal speeldagen per seizoen (typisch ongeveer 38-40)
-            url = f"{base_url}?saison_id={year}&spieltag={speeldag}"
-            response = requests.get(url, headers=headers)
+    for jaar in range(STARTJAAR, EINDJAAR + 1):
+        for speeldag in range(STARTSPEELDAG, EINDSPEELDAG - 1):  # Maximaal aantal speeldagen per seizoen (typisch ongeveer 38-40)
+
+            url = f"{URL}?saison_id={jaar}&spieltag={speeldag}"
+            response = requests.get(url, headers=HEADERS)
+
             if response.status_code == 200:
                 soup = BeautifulSoup(response.content, 'html.parser')
                 match_rows = soup.select(".box")  # Selecteer de juiste container voor elke wedstrijd
+
+                if not soup.find('option', selected=True, value=str(speeldag)):
+                    break  # geen speeldagen meer beschikbaar voor dit seizoen
+
                 for match in match_rows:
+
                     # Thuisploeg
                     thuisploeg = match.find('td', class_='rechts hauptlink no-border-rechts hide-for-small spieltagsansicht-vereinsname')
                     thuisploeg = thuisploeg.get_text(strip=True) if thuisploeg else None
@@ -63,21 +71,21 @@ with open('matches.csv', mode='w', newline='', encoding='utf-8') as file:
                         score_thuisploeg = None
                         score_uitploeg = None
 
-                    # Match-ID
+                    # Match_ID
                     match_id_tag = match.find('a', class_='liveLink')
                     match_id = match_id_tag['href'].split('/')[-1] if match_id_tag else None
 
                     writer.writerow({
-                        'Seizoen': year,
+                        'Match_ID': match_id,
+                        'Seizoen': f"{jaar}-{jaar+1}",
                         'Speeldag': speeldag,
                         'Datum': datum.strip() if datum else None,
                         'Tijdstip': tijdstip.strip() if tijdstip else None,
-                        'Naam thuisploeg': thuisploeg,
-                        'Resultaat thuisploeg': score_thuisploeg,
-                        'Resultaat uitploeg': score_uitploeg,
-                        'Naam uitploeg': uitploeg,
-                        'Match-ID': match_id,
+                        'Thuisploeg': thuisploeg,
+                        'ResultaatThuisploeg': score_thuisploeg,
+                        'ResultaatUitploeg': score_uitploeg,
+                        'Uitploeg': uitploeg,
                     })
-                    print(f"Wedstrijdgegevens voor seizoen {year}, speeldag {speeldag} zijn geschreven.")
+                print(f"Wedstrijdgegevens voor seizoen {jaar}, speeldag {speeldag} zijn geschreven.")
             else:
-                print(f"Fout bij het ophalen van gegevens voor seizoen {year}, speeldag {speeldag}. Statuscode: {response.status_code}")
+                print(f"Fout bij het ophalen van gegevens voor seizoen {jaar}, speeldag {speeldag}. Statuscode: {response.status_code}")
