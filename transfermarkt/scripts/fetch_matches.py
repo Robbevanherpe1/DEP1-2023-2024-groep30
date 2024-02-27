0a5414d9b43c92904bcac47c624fcd76168c050b
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-02-28 00:01:03 +0100

    Update: fetch_matches trying to find solution for missing match_id in some fields

--- transfermarkt/scripts/fetch_matches.py
+++ transfermarkt/scripts/fetch_matches.py
@@ -1,46 +1,58 @@
 import requests
-import os
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
+import re
 
 URL = "https://www.transfermarkt.be/jupiler-pro-league/spieltag/wettbewerb/BE1/plus/"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
 
 STARTJAAR = 1960
-EINDJAAR = datetime.now().year - 1
+EINDJAAR = 1960
 
 STARTSPEELDAG = 1
 EINDSPEELDAG = 50
 
+# Functie toegevoegd om match_id te extraheren
+def extract_match_id(url):
+    match_id = re.search(r'/spielbericht/index/spielbericht/(\d+)', url)
+    return match_id.group(1) if match_id else None
+
+
 with open('matches.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=[ 'Match-ID', 'Seizoen', 'Speeldag', 'Datum', 'Tijdstip',
+    writer = csv.DictWriter(file, fieldnames=[ 'Match_ID', 'Seizoen', 'Speeldag', 'Datum', 'Tijdstip',
                                                'Thuisploeg', 'Resultaat_Thuisploeg', 'Resultaat_Uitploeg', 'Uitploeg'])
     writer.writeheader()
 
     for jaar in range(STARTJAAR, EINDJAAR + 1):
-        for speeldag in range(STARTSPEELDAG, EINDSPEELDAG - 1):
+        for speeldag in range(STARTSPEELDAG, EINDSPEELDAG + 1):
+
             url = f"{URL}?saison_id={jaar}&spieltag={speeldag}"
             response = requests.get(url, headers=HEADERS)
             if response.status_code == 200:
                 
                 soup = BeautifulSoup(response.content, 'html.parser')
-                match_rows = soup.select(".box")  # Selecteer de juiste container voor elke wedstrijd
+                trs = soup.select(".box")  # Selecteer de juiste container voor elke wedstrijd
 
                 if not soup.find('option', selected=True, value=str(speeldag)):
                     break  # geen speeldagen meer beschikbaar voor dit seizoen
 
-                for match in match_rows:
+                for row in trs:
+                    # Match-ID
+                    # Implementeer de logica zoals voorheen maar gebruik de extract_match_id voor het ophalen van match_id
+                    result_link = row.find('a', class_='liveLink', href=True)
+                    match_id = extract_match_id(result_link['href']) if result_link else None
+
                     # Thuisploeg
-                    thuisploeg = match.find('td', class_='rechts hauptlink no-border-rechts hide-for-small spieltagsansicht-vereinsname')
+                    thuisploeg = row.find('td', class_='rechts hauptlink no-border-rechts hide-for-small spieltagsansicht-vereinsname')
                     thuisploeg = thuisploeg.get_text(strip=True) if thuisploeg else None
 
                     # Uitploeg
-                    uitploeg = match.find('td', class_='hauptlink no-border-links no-border-rechts hide-for-small spieltagsansicht-vereinsname')
+                    uitploeg = row.find('td', class_='hauptlink no-border-links no-border-rechts hide-for-small spieltagsansicht-vereinsname')
                     uitploeg = uitploeg.get_text(strip=True) if uitploeg else None
 
                     # Datum en tijdstip van de match
-                    datum_tijdstip_tag = match.find('td', class_='zentriert no-border')
+                    datum_tijdstip_tag = row.find('td', class_='zentriert no-border')
                     if datum_tijdstip_tag:
                         datum_tijdstip = datum_tijdstip_tag.get_text(strip=True)
                         datum, tijdstip = datum_tijdstip.split('-')
@@ -49,7 +61,7 @@ with open('matches.csv', mode='w', newline='', encoding='utf-8') as file:
                         tijdstip = None
 
                     # Resultaat van de wedstrijd
-                    resultaat_element = match.find('span', class_='matchresult finished')
+                    resultaat_element = row.find('span', class_='matchresult finished')
                     resultaat = resultaat_element.get_text(strip=True) if resultaat_element else None
 
                     # Scores splitsen
@@ -65,14 +77,10 @@ with open('matches.csv', mode='w', newline='', encoding='utf-8') as file:
                         score_thuisploeg = None
                         score_uitploeg = None
                    
-                    # Match-ID
-                    match_id_tag = match.find('a', class_='liveLink')
-                    match_id = match_id_tag['href'].split('/')[-1] if match_id_tag else None
                    
-
                     if any([ match_id, datum, tijdstip, thuisploeg, score_thuisploeg, score_uitploeg, uitploeg]):
                         writer.writerow({
-                            'Match-ID': match_id,
+                            'Match_ID': match_id,
                             'Seizoen': f"{jaar}-{jaar+1}",
                             'Speeldag': speeldag,
                             'Datum': datum.strip() if datum else None,
