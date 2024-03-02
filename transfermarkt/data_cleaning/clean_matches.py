0c27244619c31c47d79be645f8ee1072454c846d
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-02 10:45:24 +0100

    Update: clean_matches

---
+++ transfermarkt/data_cleaning/clean_matches.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def clean_data(file_path):
+    # Load the CSV file with a specified encoding to handle non-UTF-8 characters
+    try:
+        data = pd.read_csv(file_path, encoding='utf-8')
+    except UnicodeDecodeError:
+        data = pd.read_csv(file_path, encoding='ISO-8859-1')
+    
+    # data cleaning
+    
+    return data
+
+# Example usage
+file_path = r'D:\Hogent\Visual Studio Code\DEP\DEP-G30\DEP1-2023-2024-groep30\transfermarkt\data\scraped_data\matches.csv'
+cleaned_data = clean_data(file_path)
+
+# Save the cleaned data to a new CSV
+cleaned_data.to_csv('matches_cleaned.csv', index=False)
