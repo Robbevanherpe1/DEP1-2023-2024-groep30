bec8e44fee0c03be90ed94ec2ae366d654da3777
Robbe <robbe.van.herpe@outlook.com>
2024-03-04 20:22:36 +0100

    Add: merged stamnummer with team

--- transfermarkt/data_controlling/stamnummers/scripts/Stamnummer_goals.py
+++ transfermarkt/data_controlling/stamnummers/scripts/Stamnummer_goals.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Laad het huidige dataset
-df_clubs = pd.read_csv(r'C:\Users\Robbe\Documents\School_jaar_2\sem2\DEP\development\cleaning\goals.csv')
+df_clubs = pd.read_csv(r'DEP-G30\transfermarkt\data\cleaned_data\goals_clean.csv')
 
 # Laad het dataset met stamnummers
-df_stamnummers = pd.read_csv(r'C:\Users\Robbe\Documents\School_jaar_2\sem2\DEP\development\cleaning\football_clubs_stamnummer.csv')
+df_stamnummers = pd.read_csv(r'DEP-G30\transfermarkt\data_controlling\stamnummers\data\stamnummer.csv')
 
 # Bereid df_stamnummers voor op samenvoegen door twee aparte DataFrames te maken voor thuisploeg en uitploeg
 df_stamnummers_thuis = df_stamnummers[['Stamnummer', 'Thuisploeg']].rename(columns={'Thuisploeg': 'Club', 'Stamnummer': 'Stamnummer_Thuis'})
@@ -17,4 +17,4 @@ df_clubs = pd.merge(df_clubs, df_stamnummers_thuis, left_on='Thuisploeg', right_
 df_clubs = pd.merge(df_clubs, df_stamnummers_uit, left_on='Uitploeg', right_on='Club', how='left').drop('Club', axis=1)
 
 # Sla de samengevoegde dataframe op naar een nieuw CSV-bestand
-df_clubs.to_csv(r'C:\Users\Robbe\Documents\School_jaar_2\sem2\DEP\development\cleaning\merged_goals_stamnummers.csv', index=False)
+df_clubs.to_csv(r'DEP-G30\transfermarkt\data_controlling\stamnummers\data\merged_goals_stamnummers.csv', index=False)
