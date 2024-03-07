d15b9a43769870e927a5e021e5c3412d26ff38be
AymanT101 <ayman.thomas@student.hogent.be>
2024-03-07 14:52:31 +0100

    Added control_matches : Speeldagen/Wedstrijden check

--- transfermarkt/data_controlling/control_matches.py
+++ transfermarkt/data_controlling/control_matches.py
@@ -1,19 +1,38 @@
 import pandas as pd
 
-def control_data(file_path):
-    # Load the CSV file with a specified encoding to handle non-UTF-8 characters
-    try:
-        data = pd.read_csv(file_path, encoding='utf-8')
-    except UnicodeDecodeError:
-        data = pd.read_csv(file_path, encoding='ISO-8859-1')
+def remove_records_with_more_matches_than_matchdays(data):
+    # Get the number of unique matchdays
+    num_matchdays = data['Speeldag'].nunique()
     
-    # control data
+    # Get the total number of matches
+    num_matches = len(data)
+    
+    # Check if there are records with more matches than matchdays
+    if num_matches > num_matchdays:
+        print("Er zijn records met meer wedstrijden dan speeldagen. Deze worden verwijderd.")
+        
+        # Filter records with more matches than matchdays
+        records_with_more_matches = data[data['Speeldag'] > num_matches / num_matchdays]
+        
+        # Remove records with more matches than matchdays
+        data = data.drop(records_with_more_matches.index)
+        
+        num_deleted_records = len(records_with_more_matches)
+        
+        if num_deleted_records == 0:
+            print("Er zijn geen records verwijderd. Data is OK.")
+        else:
+            print(f"{num_deleted_records} records zijn verwijderd.")
+    else:
+        print("Data is OK, er zijn geen records met meer wedstrijden dan speeldagen.")
     
     return data
 
 # Example usage
-file_path = r'DEP-G30\transfermarkt\data\controlled_data_stamnummer\matches_stamnummer.csv'
-cleaned_data = control_data(file_path)
+file_path = r'C:\DEP1-2023-2024-groep30\transfermarkt\data\cleaned_data\matches_clean.csv'
+cleaned_data = pd.read_csv(file_path, encoding='utf-8')
+cleaned_data = remove_records_with_more_matches_than_matchdays(cleaned_data)
 
-# Save the controlled data to a new CSV
-cleaned_data.to_csv(r'DEP-G30\transfermarkt\data\controlled_data\matches_controlled.csv', index=False)
+if cleaned_data is not None:
+    # Save the controlled data to a new CSV
+    cleaned_data.to_csv(r'C:\DEP1-2023-2024-groep30\transfermarkt\data\cleaned_data\matches_controlled.csv', index=False)
