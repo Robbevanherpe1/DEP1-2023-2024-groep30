c67ca8efab21aae02b714a3de96dd991375f7949
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-11 20:35:00 +0100

    Update: control_stand: doing more checks and using function to print them

--- transfermarkt/data_controlling/control_stand.py
+++ transfermarkt/data_controlling/control_stand.py
@@ -1,16 +1,5 @@
 import pandas as pd
 
-def validate_standings_order(group):
-    # Sorteer de groep op basis van de opgegeven criteria
-    sorted_group = group.sort_values(by=['PuntenVoor', 'AantalGewonnen', 'Doelpuntensaldo', 'DoelpuntenVoor'], 
-                                     ascending=[False, False, False, False])
-    # Genereer een nieuwe 'CorrectStand' gebaseerd op de juiste volgorde
-    sorted_group['CorrectStand'] = range(1, len(sorted_group) + 1)
-    # Check of de nieuwe 'CorrectStand' overeenkomt met de originele 'Stand'
-    group['StandCorrect'] = (group['Stand'].values == sorted_group['CorrectStand'].values)
-    # Houd de clubnamen bij tijdens het sorteren voor het geval we ze nodig hebben voor rapportage
-    group['Club'] = sorted_group['Club']
-    return group
 
 def load_data(file_path, encoding_list=['utf-8', 'ISO-8859-1']):
     for encoding in encoding_list:
@@ -20,48 +9,67 @@ def load_data(file_path, encoding_list=['utf-8', 'ISO-8859-1']):
             continue
     raise ValueError(f"Failed to load the file {file_path} with provided encodings.")
 
+
+def validate_standings_order(group):
+    # Sorteer de groep op basis van de opgegeven criteria
+    sorted_group = group.sort_values(by=['PuntenVoor', 'AantalGewonnen', 'Doelpuntensaldo', 'DoelpuntenVoor'], 
+                                     ascending=[False, False, False, False])
+    
+    # Genereer een nieuwe 'CorrectStand' gebaseerd op de juiste volgorde
+    sorted_group['CorrectStand'] = range(1, len(sorted_group) + 1)
+
+    # Check of de nieuwe 'CorrectStand' overeenkomt met de originele 'Stand'
+    group['StandCorrect'] = (group['Stand'].values == sorted_group['CorrectStand'].values)
+
+    # Houd de clubnamen bij tijdens het sorteren voor het geval we ze nodig hebben voor rapportage
+    group['Club'] = sorted_group['Club']
+    return group
+
+def print_errors(data, error_type, condition):
+    error_df = data[~condition]
+    if not error_df.empty:
+        error_info = error_df[['SeizoensBegin', 'Speeldag', 'Club']].drop_duplicates()
+        for _, row in error_info.iterrows():
+            error_count = len(error_df[(error_df['SeizoensBegin'] == row['SeizoensBegin']) & (error_df['Speeldag'] == row['Speeldag'])])
+            print(f"Fouten gevonden in {error_type} voor Seizoen {row['SeizoensBegin']}, Speeldag {row['Speeldag']} voor club: {row['Club']}: {error_count} rijen")    
+
+
 def control_data(file_path):
     # Laad de CSV-bestanden
     data = load_data(file_path)
     
     # Stel het puntensysteem in
     jaarTallen2puntensysteem = set(range(1960, 1995)) - {1964}
-    data['PointsForWin'] = data['SeizoensBegin'].apply(lambda x: 2 if x in jaarTallen2puntensysteem else 3)
+    data['PuntenVoorOverwinning'] = data['SeizoensBegin'].apply(lambda x: 2 if x in jaarTallen2puntensysteem else 3)
+
+    # Controle op aantal wedstrijden
+    data['AantalWedstrijdenCorrect'] = data['AantalGewonnen'] + data['AantalGelijk'] + data['AantalVerloren'] == data['Speeldag']
 
     # Bereken verwachte punten
-    data['ExpectedPoints'] = data['AantalGewonnen'] * data['PointsForWin'] + data['AantalGelijk']
+    data['VerwachtePunten'] = data['AantalGewonnen'] * data['PuntenVoorOverwinning'] + data['AantalGelijk']
     
     # Check op correcte doelpuntensaldo
     data['CorrectDoelpuntensaldo'] = (data['DoelpuntenVoor'] - data['DoelpuntenTegen']) == data['Doelpuntensaldo']
     
     # Check op correcte verwachte punten
-    data['CorrectExpectedPoints'] = data['ExpectedPoints'] == data['PuntenVoor']
+    data['CorrectVerwachtePunten'] = data['VerwachtePunten'] == data['PuntenVoor']
 
     validated_data = data.groupby(['SeizoensBegin', 'Speeldag']).apply(validate_standings_order)
     
-    # Rapporteer fouten voor Doelpuntensaldo en Verwachte Punten
-    for error_type, error_df in [('Doelpuntensaldo', data[~data['CorrectDoelpuntensaldo']]), ('Verwachte Punten', data[~data['CorrectExpectedPoints']])]:
-        if not error_df.empty:
-            error_info = error_df[['SeizoensBegin', 'Speeldag', 'Club']].drop_duplicates()
-            for _, row in error_info.iterrows():
-                print(f"Fouten gevonden in {error_type} voor Seizoen {row['SeizoensBegin']}, Speeldag {row['Speeldag']} voor club: {row['Club']}: {len(error_df[(error_df['SeizoensBegin'] == row['SeizoensBegin']) & (error_df['Speeldag'] == row['Speeldag'])])} rijen")
+    # Fouten rapporteren
+    print_errors(data, 'DOELPUNTENSALDO', data['CorrectDoelpuntensaldo'])
+    print_errors(data, 'VERWACHTE PUNTEN', data['CorrectVerwachtePunten'])
+    print_errors(data, 'AANTAL WEDSTRIJDEN', data['AantalWedstrijdenCorrect'])
 
-    # Controleer op incorrecte standen en rapporteer deze
-    incorrect_standings = validated_data[~validated_data['StandCorrect']]
-    if not incorrect_standings.empty:
-        incorrect_info = incorrect_standings[['SeizoensBegin', 'Speeldag', 'Club']].drop_duplicates()
-        for _, row in incorrect_info.iterrows():
-            print(f"Incorrecte standen gevonden in Seizoen {row['SeizoensBegin']}, Speeldag {row['Speeldag']} voor club: {row['Club']}: {len(incorrect_standings[(incorrect_standings['SeizoensBegin'] == row['SeizoensBegin']) & (incorrect_standings['Speeldag'] == row['Speeldag'])])} rijen.")
-    else:
-        print("Alle standen zijn correct.")
-        
+    validated_data = data.groupby(['SeizoensBegin', 'Speeldag']).apply(validate_standings_order)
+    print_errors(validated_data, 'KLASSEMENT', validated_data['StandCorrect'])
+  
     return validated_data
 
 
-# Path to your cleaned CSV file
+# pad naar csv-bestand
 file_path = r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data\cleaned_data\stand_clean.csv'
-
 controlled_data = control_data(file_path)
 
-# Save the controlled data to a new CSV
+# Gecontroleerde csv opslaan
 controlled_data.to_csv(r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data\controlled_data\stand_controlled.csv', index=False)
