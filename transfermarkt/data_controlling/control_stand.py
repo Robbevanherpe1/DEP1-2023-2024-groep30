9910a2bb9a69f9410497050ddfe0472cbb94a77f
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-14 14:30:09 +0100

    update: stand_contolled: add checks

--- transfermarkt/data_controlling/control_stand.py
+++ transfermarkt/data_controlling/control_stand.py
@@ -6,17 +6,22 @@ def load_data(file_path, encoding_list=['utf-8', 'ISO-8859-1']):
             return pd.read_csv(file_path, encoding=encoding)
         except UnicodeDecodeError:
             continue
+    
     raise ValueError(f"Failed to load the file {file_path} with provided encodings.")
 
 def validate_standings_order(group):
-    sorted_group = group.sort_values(by=['PuntenVoor', 'AantalGewonnen', 'Doelpuntensaldo', 'DoelpuntenVoor'], ascending=[False, False, False, False])
+    sorted_group = group.sort_values(by=['PuntenVoor', 'AantalGewonnen', 'Doelpuntensaldo', 'DoelpuntenVoor'], 
+                                            ascending=[False, False, False, False])
+    
     group['CorrectStand'] = sorted_group['Stand'].values
     group['StandCorrect'] = (group['Stand'] == group['CorrectStand'])
+
     return group
 
 def print_errors(data, error_type):
     error_counts = data.groupby(['SeizoensBegin', 'Speeldag', 'Club']).size().reset_index(name='AantalFouten')
     error_counts['FoutType'] = error_type
+
     return error_counts
 
 def control_data(file_path):
@@ -29,7 +34,6 @@ def control_data(file_path):
 
     # Geen enkel record met meer wedstrijden dan speeldagen
     data['GeenEnkelRecordMeerWedstrijdenDanSpeeldagen'] = data['Speeldag'] >= data['AantalGespeeld']
-
     
     # Bereken verwachte punten
     data['VerwachtePunten'] = data['AantalGewonnen'] * data['PuntenVoorOverwinning'] + data['AantalGelijk']
@@ -40,6 +44,15 @@ def control_data(file_path):
     # Check op correcte verwachte punten
     data['CorrectVerwachtePunten'] = data['VerwachtePunten'] == data['PuntenVoor']
 
+    # Uniek stamnmmer per club
+    data['UniekeStamnummerPerClub'] = data.duplicated(subset=['Club', 'Stamnummer'], keep=False)
+
+    # Consistentie SeizoensBegin en SeizoensEinde
+    data['ConsistentSeizoen'] = data['SeizoensEinde'] > data['SeizoensBegin']
+
+    # Correct aantal gespeelde wedstrijden (AantalGespeeld = AantalGewonnen + AantalGelijk + AantalVerloren)
+    data['AantalGespeeld'] == (data['AantalGewonnen'] + data['AantalGelijk'] + data['AantalVerloren'])
+
     errors = pd.DataFrame()
 
     # Fouten rapporteren
@@ -47,6 +60,9 @@ def control_data(file_path):
         (data['GeenEnkelRecordMeerWedstrijdenDanSpeeldagen'], 'MEER WEDSTRIJDEN DAN SPEELDAGEN'),
         (data['CorrectDoelpuntensaldo'], 'DOELPUNTENSALDO INCORRECT'),
         (data['CorrectVerwachtePunten'], 'VERWACHTE PUNTEN INCORRECT'),
+        (data['UniekeStamnummerPerClub'], 'GEEN UNEIK STAMNUMMER PER CLUB'),
+        (data['ConsistentSeizoen'], 'INCONSISTENT SEIZOEN'),
+        (data['AantalGespeeld'], 'AANTAL GESPEELDE WEDSTRIJDEN INCORRECT'),
     ]:
     
         errors = pd.concat([errors, print_errors(data[~condition], error_type)])
