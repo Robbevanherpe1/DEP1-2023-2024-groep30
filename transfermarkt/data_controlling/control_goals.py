dd9bdc0e19e8cdb1189d7eac858d25bd75460477
Robbe <robbe.van.herpe@outlook.com>
2024-03-17 20:07:27 +0100

    fix : data goals

--- transfermarkt/data_controlling/control_goals.py
+++ transfermarkt/data_controlling/control_goals.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Bestanden inlezen
-wedstrijden_df = pd.read_csv('wedstrijden.csv')
-doelpunten_df = pd.read_csv('doelpunten.csv')
+wedstrijden_df = pd.read_csv('matches_clean2.csv')
+doelpunten_df = pd.read_csv('goals_clean3.csv')
 
 # Datum- en tijdstipformaten corrigeren en samenvoegen tot één datetime kolom
 wedstrijden_df['DateTime'] = pd.to_datetime(wedstrijden_df['Datum'] + ' ' + wedstrijden_df['Tijdstip'])
@@ -11,20 +11,13 @@ doelpunten_df['GoalDateTime'] = pd.to_datetime(doelpunten_df['Datum'] + ' ' + do
 # Controleer doelpunttijdstippen binnen 4 uur na start van de match
 doelpunten_df = doelpunten_df.merge(wedstrijden_df[['Match_ID', 'DateTime']], on='Match_ID', how='left')
 doelpunten_df['TimeDelta'] = (doelpunten_df['GoalDateTime'] - doelpunten_df['DateTime']).dt.total_seconds() / 3600
-valid_goals = doelpunten_df[(doelpunten_df['TimeDelta'] >= 0) & (doelpunten_df['TimeDelta'] <= 4)]
 invalid_goals = doelpunten_df[(doelpunten_df['TimeDelta'] < 0) | (doelpunten_df['TimeDelta'] > 4)]
 
 # Controleer overeenkomst van datums tussen wedstrijden en doelpunten
-doelpunten_df['DateMatch'] = doelpunten_df['DateTime'].dt.date
-doelpunten_df['DateGoal'] = doelpunten_df['GoalDateTime'].dt.date
-date_mismatches = doelpunten_df[doelpunten_df['DateMatch'] != doelpunten_df['DateGoal']]
-
+merged_df = doelpunten_df.merge(wedstrijden_df[['Match_ID', 'Datum']], on='Match_ID', suffixes=('_Goal', '_Match'))
+date_mismatches = merged_df[merged_df['Datum_Goal'] != merged_df['Datum_Match']]
 
 # Resultaten opslaan
-
-# Geldige doelpunten zonder ongewenste kolommen
-valid_goals_output = valid_goals.drop(columns=['GoalDateTime', 'DateTime', 'TimeDelta'])
-valid_goals_output.to_csv('valid_goals.csv', index=False)
 
 # Ongeldige doelpunten zonder ongewenste kolommen
 invalid_goals_output = invalid_goals.drop(columns=['GoalDateTime', 'DateTime', 'TimeDelta'])
