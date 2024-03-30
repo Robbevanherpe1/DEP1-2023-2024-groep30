bddcd33ae0074815061016eb8b879e4373458099
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-30 16:24:00 +0100

    Update: merge_wedstrijden: ervoor zorgen dat elke wedstrijd de juiste speeldag en het juiste seizoen krijgt obv de gegeven csv

--- transfermarkt/data_merge/merge_wedstrijden.py
+++ transfermarkt/data_merge/merge_wedstrijden.py
@@ -17,27 +17,28 @@ def merge_data(file_path_theoretisch_speeldagen, file_path_wedstrijden):
     # Pas kolomnamen in theoretische_speeldagen aan naar hoofdletters
     theoretische_speeldagen.columns = [col.capitalize() for col in theoretische_speeldagen.columns]
 
+    # Verwijder kolommen uit wedstrijden die ook in theoretische_speeldagen voorkomen (indien aanwezig)
+    for col in ['Seizoen', 'Speeldag']:
+        if col in wedstrijden.columns:
+            wedstrijden.drop(col, axis=1, inplace=True)
+
     # Zet de datums om naar hetzelfde formaat
-    theoretische_speeldagen['Datum'] = pd.to_datetime(theoretische_speeldagen['Datum'])
-    wedstrijden['Datum'] = pd.to_datetime(wedstrijden['Datum'])
+    theoretische_speeldagen['Datum'] = pd.to_datetime(theoretische_speeldagen['Datum'], format='%Y/%m/%d')
+    wedstrijden['Datum'] = pd.to_datetime(wedstrijden['Datum'], format='%Y-%m-%d')
 
     # Sorteer theoretische speeldagen op datum voor efficiënte zoekopdrachten
     theoretische_speeldagen = theoretische_speeldagen.sort_values(by='Datum')
     
-    # Merge logica: voeg 'Seizoen' en 'Speeldag' toe aan wedstrijden
-    merged_data = pd.merge_asof(wedstrijden.sort_values('Datum'), theoretische_speeldagen, left_on='Datum', right_on='Datum', direction='backward')
-
-    # Verwijder eventueel dubbele kolommen 'Seizoen' en 'Speeldag' uit de oorspronkelijke wedstrijden data
-    if 'Seizoen_x' in merged_data.columns and 'Speeldag_x' in merged_data.columns:
-        merged_data.drop(columns=['Seizoen_y', 'Speeldag_y'], inplace=True)
-        merged_data.rename(columns={'Seizoen_x': 'Seizoen', 'Speeldag_x': 'Speeldag'}, inplace=True)
+    # Merge logica: gebruik 'forward' om de speeldag en seizoen van de eerstvolgende datum in theoretische_speeldagen toe te wijzen
+    merged_data = pd.merge_asof(wedstrijden.sort_values('Datum'), theoretische_speeldagen,
+                                on='Datum', direction='forward')
 
     return merged_data
 
 # Bestandspaden voor csv-bestanden
 file_path_theoretisch_speeldagen = r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data_merge\theoretische_speeldagen.csv'
 file_path_wedstrijden = r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data_merge\wedstrijden.csv'
-file_path_merged_data = r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data\correcte_data\wedstrijden2.csv'
+file_path_merged_data = r'D:\Hogent\Visual Studio Code\DEP\DEP1-2023-2024-groep30\transfermarkt\data\correcte_data\wedstrijden.csv'
 
 # Uitvoeren van de functie
 merged_data = merge_data(file_path_theoretisch_speeldagen, file_path_wedstrijden)
