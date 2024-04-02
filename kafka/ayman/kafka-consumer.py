d867c7985e223bef6ae5db6ba6c8dd2990fe4720
AymanT101 <ayman.thomas@student.hogent.be>
2024-04-02 21:58:13 +0200

    Kafka Aanpassingen

--- kafka/ayman/kafka-consumer.py
+++ kafka/ayman/kafka-consumer.py
@@ -4,71 +4,83 @@ import csv
 from io import StringIO
 from datetime import datetime
 
-# Configure consumer 
-conf = {'bootstrap.servers': 'localhost:9092', 'group.id': 'mygroup', 'auto.offset.reset': 'earliest'}
-consumer = Consumer(conf)
+def configureer_consumer():
+    conf = {'bootstrap.servers': 'localhost:9092', 'group.id': 'mygroup', 'auto.offset.reset': 'earliest'}
+    consumer = Consumer(conf)
+    consumer.subscribe(['weddenschap_winstkansen'])
+    return consumer
 
-# Subscribe to the Kafka topic
-consumer.subscribe(['weddenschap_winstkansen'])
+def configureer_database_verbinding():
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'LAPTOP-1HKCQFQU',
+        'database': 'DEP_DWH_G30',
+        'raise_on_warnings': True
+    }
+    return mysql.connector.connect(**config)
 
-# Configure database connection
-config = {
-    'user': 'root',
-    'password': 'root',
-    'host': 'LAPTOP-1HKCQFQU',
-    'database': 'DEP_DWH_G30',
-    'raise_on_warnings': True
-}
-
-# Connect to the database
-db_conn = mysql.connector.connect(**config)
-db_cursor = db_conn.cursor()
-
-try:
-    while True:
-        msg = consumer.poll(1.0)
-
-        if msg is None:
+def verwerk_bericht(msg, db_cursor):
+    data = msg.value().decode("utf-8")
+    print(f'Bericht ontvangen: {data}')
+    
+    reader = csv.reader(StringIO(data))
+    for row in reader:
+        # Controleer of de eerste element 'ID' is, wat aangeeft dat we de kolomkoppen hebben bereikt
+        if row[0] == 'ID':
             continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            else:
-                print(msg.error())
-                break
-
-        # Process the message
-        data = msg.value().decode("utf-8")
-        print(f'Received message: {data}')
         
-        # Use the csv module to parse the CSV data
-        reader = csv.reader(StringIO(data))
-        for row in reader:
-            id, wedstrijd, starttijd, thuisploeg, uitploeg, vraag, keuze, kans, *timestamp_str = row
-            
-            # Correctly format start time
-            starttijd = starttijd.replace('T', ' ').replace('Z', '')
-            
-            # If timestamp_str is not empty, parse it; otherwise, use the current timestamp
-            if timestamp_str:
+        id, wedstrijd, starttijd, thuisploeg, uitploeg, vraag, keuze, kans, *timestamp_str = row
+        
+        # Formeer de starttijd correct
+        starttijd = starttijd.replace('T', ' ').replace('Z', '')
+        
+        # Als timestamp_str niet leeg is, parse het; anders gebruik de huidige tijd
+        if timestamp_str:
+            if timestamp_str[0]:
                 timestamp = datetime.strptime(timestamp_str[0], '%d-%m-%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
             else:
-                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Use current timestamp as default
-            
-            # Insert data into DWH 
-            sql_query = f"""
-            INSERT INTO weddenschap_winstkansen (ID, Wedstrijd, Starttijd, Thuisploeg, Uitploeg, Vraag, Keuze, Kans, Timestamp)
-            VALUES ('{id}', '{wedstrijd}', '{starttijd}', '{thuisploeg}', '{uitploeg}', '{vraag}', '{keuze}', {kans}, '{timestamp}');
-            """
-            db_cursor.execute(sql_query)
+                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        # Voeg gegevens toe aan DWH
+        sql_query = f"""
+        INSERT INTO weddenschap_winstkansen (ID, Wedstrijd, Starttijd, Thuisploeg, Uitploeg, Vraag, Keuze, Kans, Timestamp)
+        VALUES ('{id}', '{wedstrijd}', '{starttijd}', '{thuisploeg}', '{uitploeg}', '{vraag}', '{keuze}', {kans}, '{timestamp}');
+        """
+        db_cursor.execute(sql_query)
+
+def hoofdfunctie():
+    consumer = configureer_consumer()
+    db_conn = configureer_database_verbinding()
+    db_cursor = db_conn.cursor()
+
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+
+            verwerk_bericht(msg, db_cursor)
             db_conn.commit()
 
-except KeyboardInterrupt:
-    pass
+    except KeyboardInterrupt:
+        pass
 
-finally:
-    # Close Consumer
-    consumer.close()
-    # Close database connection
-    db_cursor.close()
-    db_conn.close()
+    finally:
+        # Sluit Consumer
+        consumer.close()
+        # Sluit database verbinding
+        db_cursor.close()
+        db_conn.close()
+
+if __name__ == "__main__":
+    hoofdfunctie()
--- kafka/ayman/kafka-producer.py
+++ kafka/ayman/kafka-producer.py
@@ -1,27 +1,31 @@
 import csv
 from confluent_kafka import Producer
 
-# Configureer de producer
-conf = {'bootstrap.servers': 'localhost:9092'}
-producer = Producer(conf)
+def configureer_producer():
+    conf = {'bootstrap.servers': 'localhost:9092'}
+    return Producer(conf)
 
-def delivery_report(err, msg):
+def leveringsrapport(err, msg):
     if err is not None:
         print(f'Fout bij het verzenden van bericht: {err}')
     else:
         print(f'Bericht succesvol verzonden naar {msg.topic()}')
 
-# Specificeer het pad naar je CSV-bestand
-csv_file_path = 'bets.csv'
+def verwerk_csv_bestand(producer, csv_file_path):
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            # Converteer de rij naar een string (of een ander geschikt formaat)
+            row_str = ','.join(row)
+            # Verzend de rij naar het Kafka-topic
+            producer.produce('weddenschap_winstkansen', key='key', value=row_str, callback=leveringsrapport)
 
-# Open het CSV-bestand
-with open(csv_file_path, 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        # Converteer de rij naar een string (of een andere geschikte formaat)
-        row_str = ','.join(row)
-        # Verzend de rij naar het Kafka-topic
-        producer.produce('weddenschap_winstkansen', key='key', value=row_str, callback=delivery_report)
+def hoofdfunctie():
+    producer = configureer_producer()
+    csv_file_path = 'bets.csv'
+    verwerk_csv_bestand(producer, csv_file_path)
+    # Wacht op alle verzonden berichten te worden bevestigd
+    producer.flush()
 
-# Wacht op alle verzonden berichten te worden bevestigd
-producer.flush()
+if __name__ == "__main__":
+    hoofdfunctie()
